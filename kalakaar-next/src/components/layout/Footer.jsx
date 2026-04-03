a01cd875eaa5d5ef6cb50460387e10acd1f94eea
jayeshpal001 <jayashpal5@gmail.com>
2026-04-04 02:02:45 +0530

    logo size increased in footer

--- kalakaar-next/src/components/layout/Footer.jsx
+++ kalakaar-next/src/components/layout/Footer.jsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="w-full py-12 mt-5 border-t border-white/10 flex flex-col items-center justify-center bg-black/50 backdrop-blur-md">
-      <div className="relative w-12 h-12 mb-6 opacity-40 hover:opacity-100 transition-opacity duration-500 cursor-pointer">
-        <Image 
-          src="/logo.png" 
-          alt="Kalakaar Ventures" 
-          fill 
-          className="object-contain invert" 
+    <footer className="w-full pt-5 pb-15 mt-5 border-t border-white/10 flex flex-col items-center justify-center bg-black/50 backdrop-blur-md">
+      <div className="relative w-25 h-25 md:w-33 md:h-33  opacity-40 hover:opacity-100 transition-opacity duration-500 cursor-pointer">
+        <Image
+          src="/logo.png" // Aapka main stacked logo
+          alt="Kalakaar Ventures"
+          fill
+          className="object-contain invert brightness-200 drop-shadow-[0_0_20px_rgba(255,255,255,0.15)]"
+          priority
         />
       </div>
       <p className="text-xs text-neutral-600 tracking-[0.3em] font-bold uppercase">
